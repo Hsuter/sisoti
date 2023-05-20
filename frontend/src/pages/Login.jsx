1fe15aa1beed29b6a8b7c0235359781805cbb9ee
Harry Suter <harry.suter.hs@gmail.com>
2023-05-20 19:24:19 +0300

    to

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -89,7 +89,7 @@ const Login = () => {
                     </label>
                   </div>
                   <Link
-                    href="/reqrespass"
+                    to="/reqrespass"
                     className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
                   >
                     Forgot password?
